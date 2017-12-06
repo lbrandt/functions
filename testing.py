2d205cccbc5840582d1867307bd8c088c57c6dd7
lbrandt <lennart@rohde-brandt.de>
2017-12-06 18:09:31 +0100

    Testing optional arguments in function.

--- testing.py
+++ testing.py
@@ -16,6 +16,43 @@ x.mean()
 plt.plot(x)
 #
 
-fun_ecdf(x)
+from functions.fun_ecdf import fun_ecdf
 
-jnum = 50
+x_ecdf = fun_ecdf(x)
+
+from functions.fun_hist import fun_hist
+
+x_hist1 = fun_hist(x, jnum = 10)
+x_hist2 = fun_hist(x, jnum = 20, h = 2)
+x_hist3 = fun_hist(x) # Should abort with error.
+
+
+# Learning how to use optional arguments
+def test_var_kwargs(farg, **kwargs):
+    print(farg)
+    print(kwargs)
+    print(kwargs.keys)
+    for key in kwargs:
+        print(key, kwargs[key])
+
+def test_fun(farg, **options):
+    import sys
+    
+    print(farg)
+    if options.__contains__("h"):
+        if options.__contains__	("jnum"): print("Both exist, use h, override jnum")
+        h = options.get("h")
+        print(h)
+    elif (options.__contains__	("jnum") & (options.__contains__("h")==0)):
+        jnum = options.get("jnum")
+        print(jnum)
+    else:
+        sys.exit("Not enough inputs. Need either h or jnum.")
+    
+    print("This is the end.")
+
+test_fun(1)
+            
+
+
+test_var_kwargs(farg=1, myarg2="two", myarg3=3)
